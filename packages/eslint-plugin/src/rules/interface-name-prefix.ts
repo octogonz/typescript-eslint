0e6b2bc9119d98d1c3ead4a96de78cee379ba804
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2019-08-02 21:02:32 -0700

    WIP

--- packages/eslint-plugin/src/rules/interface-name-prefix.ts
+++ packages/eslint-plugin/src/rules/interface-name-prefix.ts
@@ -1,7 +1,39 @@
 import * as util from '../util';
 
-type Options = ['never' | 'always'];
+type ParsedOptions = {
+  allowPrefix: false;
+} | {
+  allowPrefix: true;
+  allowUnderscorePrefix: boolean;
+};
+type Options = [
+  | 'never'
+  | 'always'
+  | {
+    allowPrefix?: false;
+  }
+  | {
+    allowPrefix: true;
+    allowUnderscorePrefix?: boolean;
+  },
+];
 type MessageIds = 'noPrefix' | 'alwaysPrefix';
+
+/**
+ * Parses a given value as options.
+ */
+function parseOptions([options]: Options): ParsedOptions {
+  if (options === 'never') {
+    return { allowPrefix: false, allowUnderscorePrefix: false };
+  }
+  if (options === 'always') {
+    return { allowPrefix: true, allowUnderscorePrefix: false };
+  }
+  if (!options.allowPrefix) {
+    return { allowPrefix: false, allowUnderscorePrefix: false };
+  }
+  return { allowPrefix: true, allowUnderscorePrefix: options.allowUnderscorePrefix };
+}
 
 export default util.createRule<Options, MessageIds>({
   name: 'interface-name-prefix',
@@ -18,13 +50,48 @@ export default util.createRule<Options, MessageIds>({
     },
     schema: [
       {
-        enum: ['never', 'always'],
+        oneOf: [
+          {
+            enum: [
+              // Deprecated, equivalent to: { allowPrefix: false }
+              'never',
+              // Deprecated, equivalent to: { allowPrefix: true, allowUnderscorePrefix: false }
+              'always'
+            ],
+          },
+          {
+            type: 'object',
+            properties: {
+              allowPrefix: {
+                type: 'boolean',
+                enum: [ false ],
+                default: false,
+              },
+            }
+          },
+          {
+            type: 'object',
+            properties: {
+              allowPrefix: {
+                type: 'boolean',
+                enum: [ true ]
+              },
+              allowUnderscorePrefix: {
+                type: 'boolean',
+                default: false
+              }
+            },
+            required: [ 'allowPrefix' ],  // required to select this "oneOf" alternative
+          },
+        ],
       },
     ],
   },
-  defaultOptions: ['never'],
-  create(context, [option]) {
-    const never = option !== 'always';
+  defaultOptions: [
+    { allowPrefix: false }
+  ],
+  create(context, [options]) {
+    const parsedOptions = parseOptions([options]);
 
     /**
      * Checks if a string is prefixed with "I".
@@ -35,24 +102,45 @@ export default util.createRule<Options, MessageIds>({
         return false;
       }
 
+      return /^I[A-Z]/.test(name);
+    }
+
+    /**
+     * Checks if a string is prefixed with "I" or "_I".
+     * @param name The string to check
+     */
+    function isPrefixedWithIOrUnderscoreI(name: string): boolean {
+      if (typeof name !== 'string') {
+        return false;
+      }
+
       return /^_?I[A-Z]/.test(name);
     }
 
     return {
       TSInterfaceDeclaration(node): void {
-        if (never) {
-          if (isPrefixedWithI(node.id.name)) {
+        if (parsedOptions.allowPrefix) {
+          if (isPrefixedWithIOrUnderscoreI(node.id.name)) {
             context.report({
               node: node.id,
               messageId: 'noPrefix',
             });
           }
         } else {
-          if (!isPrefixedWithI(node.id.name)) {
-            context.report({
-              node: node.id,
-              messageId: 'alwaysPrefix',
-            });
+          if (parsedOptions.allowUnderscorePrefix) {
+            if (!isPrefixedWithIOrUnderscoreI(node.id.name)) {
+              context.report({
+                node: node.id,
+                messageId: 'alwaysPrefix',
+              });
+            }
+          } else {
+            if (!isPrefixedWithI(node.id.name)) {
+              context.report({
+                node: node.id,
+                messageId: 'alwaysPrefix',
+              });
+            }
           }
         }
       },
